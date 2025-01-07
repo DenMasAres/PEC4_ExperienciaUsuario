0fa83cc2e58e17d1c3fea67b33d905b47989adc2
Denis <dmasedaa@uoc.edu>
2025-01-07 20:53:28 +0100

    programar el mixxer y añadir el codigo correspondiente

--- Assets/Scripts/HUDLevelController.cs
+++ Assets/Scripts/HUDLevelController.cs
@@ -1,64 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Rendering;
 
 public class HUDLevelController : MonoBehaviour
 {
 	[Header("Audio")]
 	[SerializeField] private AudioMixer audioMixer;
-	private int currentMasterVolume = 10;
+	[SerializeField] private TextMeshProUGUI masterVolumeText;
+	[SerializeField] private TextMeshProUGUI BSOVolumeText;
+	[SerializeField] private TextMeshProUGUI SFXVolumeText;
+    private int currentMasterVolume = 10;
 	private int currentBSOVolume = 10;
 	private int currentSFXVolume = 10;
 
-    private void Update()
-    {
+	[Header("PlayerStats")]
+    [SerializeField] private TextMeshProUGUI playerSpeedText;
+    [SerializeField] private TextMeshProUGUI playerRotationText;
 
-		if(Input.GetKeyDown(KeyCode.P))
-		//print(Mathf.Log10(1) * 20);
-		audioMixer.SetFloat("Volume", Mathf.Log10(0.1f) * 20);
-	}
 
 
     #region - AudioMixerController -
 
     public void IncreaseMasterVolume()
 	{
-		if (currentMasterVolume < 10) currentMasterVolume ++ ;
+		if (currentMasterVolume < 10)
+		{
+			currentMasterVolume++;
+            audioMixer.SetFloat("MasterVolume",  GetValue(currentMasterVolume));
+        }
+
 	}
 
 	public void DecreaseMasterVolume()
 	{
-		if (currentMasterVolume > 0) currentMasterVolume -- ;
+		if (currentMasterVolume > 0)
+		{
+			currentMasterVolume--;
+            audioMixer.SetFloat("MasterVolume", GetValue(currentMasterVolume));
+        }
 	}
 
 	public void IncreaseBSOVolume()
 	{
-		if (currentBSOVolume < 10) currentBSOVolume++;
+		if (currentBSOVolume < 10)
+		{
+			currentBSOVolume++;
+            audioMixer.SetFloat("BSOVolume", GetValue(currentBSOVolume));
+        }
 	}
 
 	public void DecreaseBSOVolume()
 	{
-		if (currentBSOVolume > 0) currentBSOVolume--;
+		if (currentBSOVolume > 0)
+		{
+			currentBSOVolume--;
+            audioMixer.SetFloat("BSOVolume", GetValue(currentBSOVolume));
+        }
 	}
 
 	public void IncreaseSFXVolume()
 	{
-		if (currentSFXVolume < 10) currentSFXVolume++;
+		if (currentSFXVolume < 10)
+		{
+			currentSFXVolume++;
+            audioMixer.SetFloat("SFXVolume", GetValue(currentBSOVolume));
+        }
 	}
 
 	public void DecreaseSFXVolume()
 	{
-		if (currentSFXVolume > 0) currentSFXVolume--;
+		if (currentSFXVolume > 0)
+		{
+			currentSFXVolume--;
+            audioMixer.SetFloat("SFXVolume", GetValue(currentBSOVolume));
+        }
 	}
 
+	private float GetValue(float value)
+	{
+		return Remap(value, 0f, 10f, -80f, 0f);
+    }
 
-	#endregion
+    private float Remap(float value, float inputMin, float inputMax, float outputMin, float outputMax)
+    {
+        return (value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin;
+    }
 
 
-	#region - CharacterParameters
+    #endregion
 
-	public void IncreasePlayerSpeed()
+
+    #region - CharacterParameters
+
+    public void IncreasePlayerSpeed()
 	{
 
 	}
@@ -78,11 +115,12 @@ public class HUDLevelController : MonoBehaviour
 
     }
 
-	public void PlayCredits()
-	{
-
-	}
-
     #endregion
+
+    public void PlayCredits()
+    {
+
+    }
+
 
 }
