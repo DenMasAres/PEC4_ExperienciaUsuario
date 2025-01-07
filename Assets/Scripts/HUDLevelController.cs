8e85d95253552527959b3b4335b08c8bc809c1cd
Denis <dmasedaa@uoc.edu>
2025-01-07 16:12:39 +0100

    añadir más contenido

--- Assets/Scripts/HUDLevelController.cs
+++ Assets/Scripts/HUDLevelController.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class HUDLevelController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	[Header("Audio")]
+	[SerializeField] private AudioMixer audioMixer;
+	private int currentMasterVolume = 10;
+	private int currentBSOVolume = 10;
+	private int currentSFXVolume = 10;
+
+
+    #region - AudioMixerController -
+
+    public void IncreaseMasterVolume()
+	{
+
+	}
+
+	public void DecreaseMasterVolume()
+	{
+
+	}
+
+	public void IncreaseBSOVolume()
+	{
+
+	}
+
+	public void DecreaseBSOVolume()
+	{
+
+	}
+
+	public void IncreaseSFXVolume()
+	{
+
+	}
+
+	public void DecreaseSFXVolume()
+	{
+
+	}
+
+
+	#endregion
+
+
+	#region - CharacterParameters
+
+	public void IncreasePlayerSpeed()
+	{
+
+	}
+
+    public void DecreasePlayerSpeed()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+	public void IncreasePlayerRotationSpeed()
+	{
+
+	}
+
+    public void DecreasePlayerRotationSpeed()
     {
-        
+
     }
+
+	public void PlayCredits()
+	{
+
+	}
+
+    #endregion
+
 }
